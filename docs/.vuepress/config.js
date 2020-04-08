edff974cfdd1986c3f504bbfc98210cc28bb3dd4
cz160 <1604506998@qq.com>
2020-04-08 16:08:34 +0800

    feat():修改更新时间显示

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,6 @@
 const navConf = require('./config/navConf');
 const headConf = require('./config/headConf');
+const moment = require('moment');
 module.exports = {
   title: '小昭成长记录',
   description: '小昭的个人博客',
@@ -7,8 +8,20 @@ module.exports = {
   themeConfig: {
     logo: '/assets/img/logo.jpg',
   },
+  plugins: [
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp) => {
+          const moment = require('moment')
+          moment.locale('zh-cn')
+          return moment(timestamp).fromNow()
+        }
+      }
+    ]
+  ],
   themeConfig: {
-    lastUpdated: '更新时间', 
+    lastUpdated: '更新时间',
     nav: navConf,
     sidebar: {
       '/vue/':[
